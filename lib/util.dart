636017f49a9f8454d9dd237229e4ec31699d1cb7
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-11 09:45:45 +0530

    Add Util class with initial functions

---
+++ lib/util.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+/// A collection of reusable utility functions.
+class Util {
+  /// Returns a "YEAR/MONTH/DATE" string for the given date.
+  /// For example, "2019/10/22".
+  ///
+  /// Useful for creating Firebase database node path fragments.
+  static String getYYYYMMDDDate(DateTime date) {
+    return DateFormat('yyyy/MM/dd').format(date);
+  }
+}
