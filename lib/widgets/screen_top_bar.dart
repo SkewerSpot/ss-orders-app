aac32c55fe527ff7381f0b0f061e613b4847a4f4
Anurag Bhandari <anurag.bhd@gmail.com>
2020-01-24 05:26:37 +0530

    Add validate unique code button in top bar

--- lib/widgets/screen_top_bar.dart
+++ lib/widgets/screen_top_bar.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:ss_orders/constants.dart';
 
 /// An [AppBar]-like widget typically shown at the top of a screen.
 ///
-/// Displays app's brand name.
+/// Displays app's brand name and validate unique code button.
 class ScreenTopBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: <Widget>[
+        IconButton(
+          iconSize: 30.0,
+          icon: Icon(Icons.arrow_back_ios),
+          color: Colors.transparent,
+          onPressed: () {},
+        ),
         Text(
           'Orders App',
           style: TextStyle(
             fontFamily: 'Pacifico',
             fontSize: 30.0,
           ),
+        ),
+        IconButton(
+          iconSize: 30.0,
+          icon: Icon(Icons.camera),
+          color: kThemeColorPurple,
+          onPressed: () {},
         ),
       ],
     );
